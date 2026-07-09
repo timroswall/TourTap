da8a5f43d59feeb5e0786bb8d6d9c17d6121057b
tfry <roswall.tim@gmail.com>
2026-07-10 00:57:31 +0900

    add 404 booking subdomain

--- frontend/src/router/booking.ts
+++ frontend/src/router/booking.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Booking from "@/views/Booking.vue"
+import NotFound from '@/views/NotFound.vue'
 
 const bookingRouter = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,10 +9,14 @@ const bookingRouter = createRouter({
       path: "/",
       name: "booking",
       component: Booking,
-      meta: {
-        hideNavbar: true
-      }
-    }
+      meta: { hideNavbar: true }
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: NotFound,
+      meta: { hideNavbar: true },
+    },
   ]
 })
 
